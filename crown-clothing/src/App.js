2d8bb0dc5711aebeb6f45714977610b5f0208358
Vaibhav Kumar <kumarvaibhav1602@gmail.com>
2023-07-16 21:31:10 +0530

    Added Routing and Navigation Bar

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,38 +1,27 @@
-import './categories.styles.scss'
-import Categories from './component/Categories/categories.component';
+import './categories.styles.scss';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return (
+    <div>
+      <div>
+        <h1>This is Shopping</h1>
+      </div>
+    </div>
+  )
+}
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-
-  return (
-      <Categories categories={categories}/>
-  );
+  return(
+    <Routes>
+      <Route path="/" element={<Navigation/>} >
+        <Route index element={<Home/>} />
+        <Route path='/shop' element={<Shop/>} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
